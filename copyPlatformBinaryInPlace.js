7959b9bc2e6231674a4a1cbc07d9c3562f57d43b
bikallem <gbikal@gmail.com>
2020-01-15 23:55:22 +0000

    Fixes postinstall executable copying logic

--- copyPlatformBinaryInPlace.js
+++ copyPlatformBinaryInPlace.js
@@ -1,53 +1,44 @@
 #!/usr/bin/env node
 
 var fs = require("fs");
+var path = require("path");
 
-var arch = process.arch;
-var platform = process.platform;
+var arch = process.arch === "ia32" ? "x86" : process.arch;
+var platform = process.platform === "win32" ? "win" : process.platform;
 
-if (arch === "ia32") {
-  arch = "x86";
-}
+var rootDir = __dirname;
+var sourceExePath = path.join(rootDir, `bs-let-${platform}-${arch}.exe`);
+var destExePath = path.join(rootDir, `ppx.exe`);
 
-if (platform === "win32") {
-  platform = "win";
-}
+copyBinary(sourceExePath, destExePath);
 
-copyBinary("bs-let-" + platform + "-" + arch + ".exe", "ppx");
+function copyBinary(sourceFilename, destFilename) {
+    var supported = fs.existsSync(sourceFilename);
 
-function copyBinary(filename, destFilename) {
-  var supported = fs.existsSync(filename);
+    if (!supported) {
+        console.error("bs-let does not support this platform :(");
+        console.error("");
+        console.error("bs-let comes prepacked as built binaries to avoid large");
+        console.error("dependencies at build-time.");
+        console.error("");
+        console.error("If you want bs-let to support this platform natively,");
+        console.error(
+            "please open an issue at our repository, linked above. Please"
+        );
+        console.error("specify that you are on the " + platform + " platform,");
+        console.error("on the " + arch + " architecture.");
+    }
 
-  if (!supported) {
-    console.error("bs-let does not support this platform :(");
-    console.error("");
-    console.error("bs-let comes prepacked as built binaries to avoid large");
-    console.error("dependencies at build-time.");
-    console.error("");
-    console.error("If you want bs-let to support this platform natively,");
-    console.error(
-      "please open an issue at our repository, linked above. Please"
-    );
-    console.error("specify that you are on the " + platform + " platform,");
-    console.error("on the " + arch + " architecture.");
-  }
-
-  if (!fs.existsSync(destFilename)) {
-    copyFileSync(filename, destFilename);
-    fs.chmodSync(destFilename, 0755);
-  }
-
-  var destFilenameExe = destFilename + ".exe";
-  if (!fs.existsSync(destFilenameExe)) {
-    copyFileSync(filename, destFilenameExe);
-    fs.chmodSync(destFilenameExe, 0755);
-  }
+    if (!fs.existsSync(destFilename)) {
+        copyFileSync(sourceFilename, destFilename);
+        fs.chmodSync(destFilename, 0755);
+    }
 }
 
 function copyFileSync(source, dest) {
-  if (typeof fs.copyFileSync === "function") {
-    fs.copyFileSync(source, dest);
-  } else {
-    fs.writeFileSync(dest, fs.readFileSync(source));
-  }
+    if (typeof fs.copyFileSync === "function") {
+        fs.copyFileSync(source, dest);
+    } else {
+        fs.writeFileSync(dest, fs.readFileSync(source));
+    }
 }
