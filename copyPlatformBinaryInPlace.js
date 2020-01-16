4098fb45c1a71b5222719af17509ad679eceee87
bikallem <gbikal@gmail.com>
2020-01-16 01:10:30 +0000

    Fixes formatting. Uncomment pipeline step

--- copyPlatformBinaryInPlace.js
+++ copyPlatformBinaryInPlace.js
@@ -13,32 +13,32 @@ var destExePath = path.join(rootDir, `ppx.exe`);
 copyBinary(sourceExePath, destExePath);
 
 function copyBinary(sourceFilename, destFilename) {
-    var supported = fs.existsSync(sourceFilename);
+  var supported = fs.existsSync(sourceFilename);
 
-    if (!supported) {
-        console.error("bs-let does not support this platform :(");
-        console.error("");
-        console.error("bs-let comes prepacked as built binaries to avoid large");
-        console.error("dependencies at build-time.");
-        console.error("");
-        console.error("If you want bs-let to support this platform natively,");
-        console.error(
-            "please open an issue at our repository, linked above. Please"
-        );
-        console.error("specify that you are on the " + platform + " platform,");
-        console.error("on the " + arch + " architecture.");
-    }
+  if (!supported) {
+    console.error("bs-let does not support this platform :(");
+    console.error("");
+    console.error("bs-let comes prepacked as built binaries to avoid large");
+    console.error("dependencies at build-time.");
+    console.error("");
+    console.error("If you want bs-let to support this platform natively,");
+    console.error(
+      "please open an issue at our repository, linked above. Please"
+    );
+    console.error("specify that you are on the " + platform + " platform,");
+    console.error("on the " + arch + " architecture.");
+  }
 
-    if (!fs.existsSync(destFilename)) {
-        copyFileSync(sourceFilename, destFilename);
-        fs.chmodSync(destFilename, 0755);
-    }
+  if (!fs.existsSync(destFilename)) {
+    copyFileSync(sourceFilename, destFilename);
+    fs.chmodSync(destFilename, 0755);
+  }
 }
 
 function copyFileSync(source, dest) {
-    if (typeof fs.copyFileSync === "function") {
-        fs.copyFileSync(source, dest);
-    } else {
-        fs.writeFileSync(dest, fs.readFileSync(source));
-    }
+  if (typeof fs.copyFileSync === "function") {
+    fs.copyFileSync(source, dest);
+  } else {
+    fs.writeFileSync(dest, fs.readFileSync(source));
+  }
 }
